e8577df9049b4652b81a38a1ec8737e14ea13bc5
pengcheng <304829842@qq.com>
2022-09-16 11:26:55 +0800

    11

---
+++ yang/assets/MainComponent.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('MainComponent')
+export class MainComponent extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
